6f3dd62ff13ae8effc5dd9b35ffad2c8279c7ebe
Tyler Jones <tylerjones@Tylers-MacBook-Pro.local>
2015-06-17 14:17:30 -0400

    added complete to migration

--- db/migrate/20150617181052_create_posts.rb
+++ db/migrate/20150617181052_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration
       t.string :image
       t.integer :user_id
       t.string :answer
+      t.boolean :complete
 
       t.timestamps null: false
     end
