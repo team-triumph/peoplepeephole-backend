f42e2caf9576ae02d2d34cc7ed86c2d0987fd3e1
Tyler Jones <tylerjones@Tylers-MacBook-Pro.local>
2015-06-17 14:11:57 -0400

    added models

---
+++ db/migrate/20150617181052_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :image
+      t.integer :user_id
+      t.string :answer
+
+      t.timestamps null: false
+    end
+  end
+end
