f05f96333e48286cecb3c134bec967b08af137a4
Tyler Jones <tylerjones@Tylers-MacBook-Pro.local>
2015-06-18 14:56:56 -0400

    added update method on guesses controller

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,4 +1,8 @@
 class Guess < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
+
+  validates :guess, :user_id, :post_id, presence: true
+  validates :guess, format: { with: /\S/,
+    message: "cannot be empty." }
 end
