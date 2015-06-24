96472b2b667630d19106fa3825ed9e97d66945c8
pwood1284@gmail.com <your_email@whatever.com>
2015-06-24 07:55:21 -0400

    add tests for posts and users

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "post must have an owner" do
+    post = Post.new
+    refute post.save, "Tried to save a post without an id"
+  end
+
+  test "confirm the owner of a post" do
+    post = posts(:post1)
+    assert_equal post.user, users(:philip)
+  end
+
 end
