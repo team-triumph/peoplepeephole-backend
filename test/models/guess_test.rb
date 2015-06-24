0ecbb6c0415db1b0fe4d9177e67e57fdd3c9862b
Tyler Jones <tylerjones@Tylers-MacBook-Pro.local>
2015-06-23 20:11:35 -0400

    added tests to user and guess model

--- test/models/guess_test.rb
+++ test/models/guess_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class GuessTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "guess isn't blank" do
+    guess = guesses(:thing)
+    assert guess.save
+  end
+
+  test "guess format is correct" do
+    guess = guesses(:thing)
+    assert guess.save
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+
+  test "can't save an empty user." do
+  user = User.new
+  refute user.save
+  end
+
+  test "user emails must be unique." do
+    tyler1 = User.new(email: "tyler@tyler.com", password: "tyler")
+    tyler2 = User.new(email: "tyler@tyler.com", password: "wordup")
+    assert tyler1.save
+    refute tyler2.save, "Tried to save user with duplicate email."
+  end
+
+  test "user input is an email address." do
+    tyler1 = User.new(email: "tylerjones", password: "tylerjones")
+    tyler2 = users(:tylerjones)
+    refute tyler1.save, "not an email address."
+    assert tyler2.save
+  end
+
+
+
 end
